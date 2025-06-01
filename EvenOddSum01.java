c1092e10d4bf7967924e3e6bad5497ac44fc5871
Ashish Kumar Mishra <ashishmishra102001@gmail.com>
2025-06-01 14:23:52 +0530

    all number sum

---
+++ EvenOddSum01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EvenOddSum01 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number;
+        int choice;
+        int evenSum=0;
+        int oddSum=0;
+
+        do {
+            System.out.println("enter the number:");
+            number =sc.nextInt();
+            if (number%2==0) {
+               evenSum+=number;
+            }else{
+                oddSum+=number;
+            }
+
+            System.out.println("Do you want to continue? Press 1 for yes or 0 for no");
+            choice=sc.nextInt();
+        } while (choice==1);
+        System.out.println(evenSum);
+        System.out.println(oddSum);
+    }
+}

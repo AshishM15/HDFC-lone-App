0cc64b8886a46bf61daf33a5d8d48a8420f79bc8
Ashish Kumar Mishra <ashishmishra102001@gmail.com>
2025-06-24 12:36:35 +0530

    modified in Sorting01 java file in my project

--- Sorting01.java
+++ Sorting01.java
@@ -23,7 +23,7 @@ public class Sorting01 {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[]={5,4,1,3,2};
+        int arr[]={5,4,1,2};
 
         bubbleSort(arr);
         printArr(arr);

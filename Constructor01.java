f6dd477a6f601a3894aa012bed6d14e09b936a0a
Ashish Kumar Mishra <ashishmishra102001@gmail.com>
2025-06-25 18:31:43 +0530

    Constructor in oops concept in java

---
+++ Constructor01.java
@@ -0,0 +1,18 @@
+public class Constructor01 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        //System.out.println(s1.name);
+    }
+
+}
+class Student {
+    String name;
+    int rollNumber;
+    Student(){
+        this.name = name;
+        System.out.println("this is called costructor in oops concepts...");
+    }
+}
+
+
+
